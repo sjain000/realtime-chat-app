6dffb96cbc4bf38cdf3b5fdd56484419aec51814
sjain000 <91184276+sjain000@users.noreply.github.com>
2023-04-23 14:36:49 +0530

    fc

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: "http://127.0.0.1:5500",
+      methods: ["GET", "POST"]
+    }
+  });
+   
+
+const users = {};
+
+io.on("connection", socket=>{
+    socket.on("new-user-joined",(name)=>{
+        
+        users[socket.id]=name;
+        socket.broadcast.emit("user-joined",name);
+    })
+    socket.on("send",(message)=>{
+        socket.broadcast.emit("recieve",{message: message , name : users[socket.id]})
+    });
+    socket.on("disconnect",(message)=>{
+        socket.broadcast.emit("left", users[socket.id])
+        delete users[socket.id];
+    })
+});
